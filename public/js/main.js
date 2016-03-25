58c775b2cc45e412698c407bbf7f71028e1d8b98
Srinivas <uisrinivas@gmail.com>
2016-03-25 11:24:35 +0530

    next generation code in JS
    i Love JS so much

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
 let hello =`custom js code goes here!!!`
 let printDate=moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(hello ,printDate);
+var odds = evens.map(v => v + 1);
+console.log(hello ,printDate,odds);
